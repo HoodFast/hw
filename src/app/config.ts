fc7fb382c9c5b9e779abb0df0d412e34aec77c8e
HoodFast <rabota-trassa@mail.ru>
2024-02-29 02:04:15 +0300

    commit

--- src/app/config.ts
+++ src/app/config.ts
@@ -6,10 +6,10 @@ export const appConfig = {
     MONGO_URL: process.env.MONGO_URL as string,
     DB_NAME: process.env.DB_NAME as string,
     DB_TYPE: process.env.DB_TYPE as string,
-    AC_SECRET: process.env.AC_SECRET as string | '321',
-    AC_TIME: process.env.AC_TIME as string | '10s',
-    RT_TIME: process.env.AC_TIME as string | '20s',
-    RT_SECRET: process.env.RT_SECRET as string | '123',
+    AC_SECRET: process.env.AC_SECRET as string,
+    AC_TIME: process.env.AC_TIME as string,
+    RT_TIME: process.env.AC_TIME as string,
+    RT_SECRET: process.env.RT_SECRET as string,
     EMAIL: process.env.EMAIL as string,
     EMAIL_PASS: process.env.EMAIL_PASS as string
 }
