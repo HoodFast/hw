cefadbc4ed59904e66579d2fbccd938ca1bccc58
HoodFast <rabota-trassa@mail.ru>
2024-03-18 18:11:26 +0300

    commit

--- src/settings.ts
+++ src/settings.ts
@@ -100,7 +100,7 @@ app.get('/', (req, res) => {
         publicationDate: publicationDate.toISOString(),
         availableResolutions: ['144']
     }
-    const test = {message: 'server is work 3.0 !'}
+    const test = {message: 'server is work 1.0 !'}
 
     res.send(test)
 })
