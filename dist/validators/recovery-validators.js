b431459c14875c4008e8133bcdffc4403034c01b
HoodFast <rabota-trassa@mail.ru>
2024-03-18 19:25:04 +0300

    commit

--- dist/validators/recovery-validators.js
+++ dist/validators/recovery-validators.js
@@ -10,6 +10,6 @@ const newPassValidator = (0, express_validator_1.body)('newPassword')
 const recoveryCodeValidator = (0, express_validator_1.body)('recoveryCode')
     .isString().withMessage('recoveryCode must be a string')
     .trim()
-    .isLength({ min: 1 }).withMessage('Incorrect recoveryCode');
+    .isLength({ min: 10 }).withMessage('Incorrect recoveryCode');
 const recoveryValidation = () => [newPassValidator, recoveryCodeValidator, input_validation_middleware_1.inputValidationMiddleware];
 exports.recoveryValidation = recoveryValidation;
--- src/validators/recovery-validators.ts
+++ src/validators/recovery-validators.ts
@@ -10,7 +10,7 @@ const newPassValidator = body('newPassword')
 const recoveryCodeValidator = body('recoveryCode')
     .isString().withMessage('recoveryCode must be a string')
     .trim()
-    .isLength({min: 1}).withMessage('Incorrect recoveryCode')
+    .isLength({min: 10}).withMessage('Incorrect recoveryCode')
 
 
 export const recoveryValidation = ()=>[newPassValidator,recoveryCodeValidator,inputValidationMiddleware]
