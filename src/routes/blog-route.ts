f17c5fdee14c549797c01fd206ef1907c57aff2c
HoodFast <rabota-trassa@mail.ru>
2024-03-26 07:43:53 +0300

    commit

--- src/routes/blog-route.ts
+++ src/routes/blog-route.ts
@@ -108,7 +108,7 @@ export class BlogsController {
         const sortData = {
             searchNameTerm: req.query.searchNameTerm ?? null,
             sortBy: req.query.sortBy ?? 'createdAt',
-            sortDirection: req.query.sortDirection ?? 'asc',
+            sortDirection: req.query.sortDirection ?? 'desc',
             pageNumber: req.query.pageNumber ? +req.query.pageNumber : 1,
             pageSize: req.query.pageSize ? +req.query.pageSize : 10
         }
